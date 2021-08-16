6e59a49e0e29adf9e264be389d35db979f1ae465
nicooterounibazo <nicolasoterounibazo@gmail.com>
2021-08-15 21:35:02 -0300

    commit productos
    cargando lista de productos

--- js/products.js
+++ js/products.js
@@ -2,5 +2,18 @@
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
 document.addEventListener("DOMContentLoaded", function (e) {
+    let datos = PRODUCTS_URL;
 
-});
+    
+    for(i = 0; i < datos.length; i++){
+    let row = "";
+    `
+    <tr>
+    <td>`+ datos[i].name +`</td>
+    <td>`+ datos[i].description + `</td>
+    <td>` + datos[i].cost + `</td>
+
+    </tr>
+    
+})
+    }
