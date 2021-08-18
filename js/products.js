6b3669ae78abda1deb68638281d28f80ffa7b1b9
nicooterounibazo <nicolasoterounibazo@gmail.com>
2021-08-18 19:09:14 -0300

    products
    cargando la lista de productos

--- js/products.js
+++ js/products.js
@@ -1,21 +1,23 @@
 //Función que se ejecuta una vez que se haya lanzado el evento de
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
-document.addEventListener("DOMContentLoaded", async function (e) {
+document.addEventListener("DOMContentLoaded",  function (e) {
     let divProducts = document.getElementById("lista");
-   let datos = await getJSONData(PRODUCTS_URL);
-    console.log(datos);
-    
-    /*for(i = 0; i < datos.length; i++){
-    /*let row = "";
-    `
-    <tr>
-    <td>`+ datos[i].name +`</td>
-    <td>`+ datos[i].description + `</td>
-    <td>` + datos[i].cost + `</td>
 
-    </tr>
-    `*/
-    });
+    for(i = 0; i < getJSONData.length; i++){
+    let contenido = {};
+    `
+    <ul>
+    <li>`+ getJSONData[i].name +`</li>
+    <li>`+ getJSONData[i].description + `</li>
+    <li>` + getJSONData[i].cost + `</li>
+    <li>` + getJSONData[i].currency + `</li>
+    <li>` + getJSONData[i].soldCount + `</li>
+
+    </ul>
+    `
+    divProducts += contenido
+    }
+})
     
     
