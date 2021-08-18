ec54643284e991d683d547032706946ba8c0ba9d
nicooterounibazo <nicolasoterounibazo@gmail.com>
2021-08-18 19:36:54 -0300

    productos

--- js/products.js
+++ js/products.js
@@ -2,22 +2,30 @@
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
 document.addEventListener("DOMContentLoaded",  function (e) {
-    let divProducts = document.getElementById("lista");
-
-    for(i = 0; i < getJSONData.length; i++){
-    let contenido = {};
+    var products = [];
+    function mostrarProductos(array){
+    var lista = "<br>"
+    for(i = 0; i < array.length; i++){
+    let contenido = array[i];
     `
     <ul>
-    <li>`+ getJSONData[i].name +`</li>
-    <li>`+ getJSONData[i].description + `</li>
-    <li>` + getJSONData[i].cost + `</li>
-    <li>` + getJSONData[i].currency + `</li>
-    <li>` + getJSONData[i].soldCount + `</li>
+    <li>`+ contenido[i].name +`</li>
+    <li>`+ contenido[i].description + `</li>
+    <li>` + contenido[i].cost + `</li>
+    <li>` + contenido[i].currency + `</li>
+    <li>` + contenido[i].soldCount + `</li>
 
     </ul>
     `
-    divProducts += contenido
-    }
-})
     
+    }
+    document.getElementById("lista").innerHTML = contenido;
+}
+document.addEventListener("DOMContentLoaded",  function (e) {
+    getJSONData(PRODUCTS_URL).then(function (result){
+        if(result.status === "ok"){
+            products = result.data;
+            mostrarProductos(products)
+        }
+    });
     
